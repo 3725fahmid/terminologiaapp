c5e227a396ace9f81a0cc70641d5f7ed26fa42e3
fahmid <fahmidhasankonok@gmail.com>
2026-01-12 16:39:06 +0600

    change some header folder data

--- app/Imports/ExcelDataImport.php
+++ app/Imports/ExcelDataImport.php
@@ -4,8 +4,10 @@ namespace App\Imports;
 
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
 
-class ExcelDataImport implements ToCollection
+class ExcelDataImport implements ToCollection, WithHeadingRow, WithChunkReading
 {
     /**
     * @param Collection $collection
@@ -14,5 +16,11 @@ class ExcelDataImport implements ToCollection
     {
         //
         return $collection;
+    }
+
+    // This prevents the server from crashing on large files
+    public function chunkSize(): int
+    {
+        return 1000;
     }
 }
